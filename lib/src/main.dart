e7b79a06a1295e1ea3e488fc2d8c52c2ef61d1a3
cacing69 <cacingworm69@gmail.com>
2019-09-23 09:38:39 +0700

    old version is support

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,8 +1,12 @@
 import 'package:terbilang/src/terbilang.dart';
+import 'package:terbilang/src/terbilang_bck.dart' as prefix0;
 
 void main() {
   Terbilang _terbilang = Terbilang(number: 999);
   print(_terbilang.result());
-  // print(_terbilang.make(number: 999));
-  // print(_terbilang.make(number: 999));
+
+
+  //OLD VERSION
+  prefix0.Terbilang _terbilang_bck = prefix0.Terbilang(number: 999);
+  print(_terbilang_bck.result());
 }
