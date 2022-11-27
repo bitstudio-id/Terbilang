b13d4beadfb533518fc1e9ab62c85dfb32dafc1c
alfian0 <alfiansyah@privvyid.tech>
2022-11-27 23:46:54 +0700

    Nullsafety

--- lib/terbilang.dart
+++ lib/terbilang.dart
@@ -15,30 +15,30 @@ class Terbilang {
   dynamic number;
 
   //the sign (-) used to join words to indicate that they have a combined
-  String hypen;
+  String? hypen;
 
   //the action or an instance of two or more events or things occurring at the same point
-  String conjunction;
+  String? conjunction;
 
   //separator.
-  String separator;
+  String? separator;
 
   //word or statement that expression on negative number
-  String negative;
+  String? negative;
 
   //word to use on decimal / fraction number
-  String decimal;
+  String? decimal;
 
   //a resource that lists the words of a language (typically in alphanumeric order) and gives their meaning, or gives the equivalent words in a different language
-  Map dictionary;
+  Map? dictionary;
 
   //a resurce that list short text on number
-  Map short;
+  Map? short;
 
-  String prefix;
-  String suffix;
+  String? prefix;
+  String? suffix;
 
-  String prenum;
+  String? prenum;
 
   String lang;
 
@@ -75,7 +75,7 @@ class Terbilang {
     return this.make(number: this.number);
   }
 
-  String make({dynamic number, String prefix, String suffix}) {
+  String make({dynamic number, String? prefix, String? suffix}) {
     // parse quoted value and separate to know what os base & fraction number
     List<String> _tmp = double.parse(number.toString()).toString().split(".");
 
@@ -88,19 +88,19 @@ class Terbilang {
 
     //make it negative and absolute number
     if (_number < 0) {
-      _string += this.negative;
+      _string += this.negative ?? "";
       _number = _abs(_number);
     }
 
     if (_number < 21) {
-      _string += this.dictionary[_number];
+      _string += this.dictionary?[_number];
     } else if (_number >= 21 && _number < 100) {
       int tens = (_number ~/ 10) * 10;
       int units = _number % 10;
 
-      _string += this.dictionary[tens];
+      _string += this.dictionary?[tens];
       if (units != 0) {
-        _string += this.hypen + this.dictionary[units];
+        _string += this.hypen ?? "" + this.dictionary?[units];
       }
     } else if (_number >= 100 && _number < 1000) {
       int hundreds = _number ~/ 100;
@@ -109,49 +109,49 @@ class Terbilang {
         String _lead = _number.toString().substring(0, 1);
         _string += (int.parse(_lead) == 1
                 ? this.prenum
-                : this.dictionary[hundreds] + " ") +
-            this.dictionary[100];
+                : this.dictionary?[hundreds] + " ") +
+            this.dictionary?[100];
       } else {
-        _string += this.dictionary[hundreds] + " " + this.dictionary[100];
+        _string += this.dictionary?[hundreds] + " " + this.dictionary?[100];
       }
 
       if (remainder != 0) {
-        _string += this.conjunction + this.make(number: remainder);
+        _string += this.conjunction ?? "" + this.make(number: remainder);
       }
     } else {
       int _log = _logBase(double.parse(_number.toString()), 1000).floor();
       num _baseUnit = pow(1000, _log);
       int _numBaseUnits = (_number ~/ _baseUnit);
-      int _remainder = _number % _baseUnit;
+      num _remainder = _number % _baseUnit;
 
       if (this.prenum != "") {
         bool _check = _numBaseUnits == 1 && _baseUnit < 1000000;
         _string +=
-            (_check ? this.prenum : this.make(number: _numBaseUnits) + " ") +
-                this.dictionary[_baseUnit];
+            (_check ? this.prenum ?? "" : this.make(number: _numBaseUnits) + " ") +
+                this.dictionary?[_baseUnit];
       } else {
         _string +=
-            this.make(number: _numBaseUnits) + ' ' + this.dictionary[_baseUnit];
+            this.make(number: _numBaseUnits) + ' ' + this.dictionary?[_baseUnit];
       }
 
       if (_remainder != 0) {
-        _string += _remainder < 100 ? this.conjunction : this.separator;
+        _string += _remainder < 100 ? this.conjunction ?? "" : this.separator ?? "";
         _string += this.make(number: _remainder);
       }
     }
 
     if (_fraction != 0) {
-      _string += this.decimal;
+      _string += this.decimal ?? "";
       _string += _buildFraction(_fraction);
     }
 
     if (this.prefix != null) {
-      _string = this.prefix + " " + _string;
+      _string = this.prefix! + " " + _string;
     }
 
     // concat current text with suffix
     if (this.suffix != null) {
-      _string = _string + " " + this.suffix;
+      _string = _string + " " + this.suffix!;
     }
 
     return _string.trim();
@@ -161,7 +161,7 @@ class Terbilang {
     String _string = "";
     args.toString().runes.forEach((e) {
       var _c = new String.fromCharCode(e);
-      _string += this.dictionary[int.parse(_c)];
+      _string += this.dictionary?[int.parse(_c)];
       _string += " ";
     });
 
